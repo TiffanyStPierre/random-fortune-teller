4a3f2bd5681eab22c42b420135dbf01e683b3f7b
TiffanyStPierre <stpierre.tiffany@gmail.com>
2022-09-09 07:45:28 -0600

    Project completed. JS function works and readme file was added

---
+++ readme.md
@@ -0,0 +1,9 @@
+# **Random Fortune Generator**
+
+## **About The Project**
+This is a basic app designed to generate a random fortune each time the button is clicked or the enter key is pressed. This app is designed for desktop use only and is not compatible with mobile devices.
+
+### **Built With**
+* HTML
+* CSS
+* Javascript
--- scripts.js
+++ scripts.js
@@ -1,18 +1,35 @@
-/* Your future is.....*/
-const messages1 = ['uncertain', 'bright', 'starting now', 'a pathway to success', 'a winding road', 'full of possibility'];
 
-/*You will do best to......*/
-const messages2 = ['act now', 'have patience', 'proceed with caution', 'find a companion', 'make a plan'];
+const paragraph = document.getElementById('fortune');
+const button = document.getElementById('btn');
 
-/*to ensure that you may.........*/
-const messages3= ['fully succeed', 'follow your dreams', 'fulfill your potential', 'take the most direct path forward', 'find true happiness'];
+const randomMessage = () => {
+    /* Your future is.....*/
+    let messages1 = ['uncertain', 'bright', 'starting now', 'a pathway to success', 'a winding road', 'full of possibility'];
 
-let randNum1 = Math.floor(Math.random() * 6);
-let randNum2 = Math.floor(Math.random() * 5);
-let randNum3 = Math.floor(Math.random() * 5);
+    /*You will do best to......*/
+    let messages2 = ['act now', 'have patience', 'proceed with caution', 'find a companion', 'make a plan'];
 
-const randomMessage = (arr1, arr2, arr3) => {
-    return `Your future is ${messages1[randNum1]}. You will do best to ${messages2[randNum2]} to ensure that you may ${messages3[randNum3]}. Good luck!`;
+    /*to ensure that you may.........*/
+    let messages3= ['fully succeed', 'follow your dreams', 'fulfill your potential', 'take the most direct path forward', 'find true happiness'];
+
+    let randNum1 = Math.floor(Math.random() * messages1.length);
+    let randNum2 = Math.floor(Math.random() * messages2.length);
+    let randNum3 = Math.floor(Math.random() * messages3.length);
+
+    fortune = `Your future is ${messages1[randNum1]}. You will do best to ${messages2[randNum2]} to ensure that you may ${messages3[randNum3]}. Good luck!`;
+    paragraph.innerHTML = fortune;
 }
 
-console.log(randomMessage(messages1, messages2, messages3));
+console.log(randomMessage());
+
+function showFortune(e) {
+    e.preventDefault();
+    randomMessage();
+}
+
+button.addEventListener('click', showFortune);
+button.addEventListener('keydown', event => {
+    if (event.key === "Enter") {
+        showFortune();
+    }
+});  
